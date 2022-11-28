428449cfed5e0173099745fa49e1cf55f8cb8d25
coding-to-music <connors.tom@gmail.com>
2022-11-28 22:13:35 +0000

    Build

--- awsS3.js
+++ awsS3.js
@@ -5,15 +5,19 @@ const NAME_OF_BUCKET = "my-site-images-test";
 // const NAME_OF_BUCKET = process.env.NAME_OF_BUCKET;
 const multer = require("multer");
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
 if (process.env.NODE_ENV !== "production") {
   AWS.config.loadFromPath("./credentials.json");
 }
-// else {
-//  make sure to set environment variables in production for:
-//  AWS_ACCESS_KEY_ID
-//  AWS_SECRET_ACCESS_KEY
-//  and aws will automatically use those environment variables
-// }
+else {
+  //  make sure to set environment variables in production for:
+  //  and aws will automatically use those environment variables
+  NAME_OF_BUCKET = process.env.NAME_OF_BUCKET;
+  AWS_ACCESS_KEY_ID = process.env.APP_AWS_ACCESS_KEY_ID;
+  AWS_SECRET_ACCESS_KEY = process.env.APP_AWS_SECRET_ACCESS_KEY;
+  MONGO_URI = process.env.MONGO_URI; 
+}
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 const singlePublicFileUpload = async (file) => {
